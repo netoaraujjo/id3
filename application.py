2d921ee916f676867d267a8cd843dbaa8c4a69e1
Hugo Piauilino <hugos94@gmail.com>
2015-11-17 12:00:33 -0300

    Adicionada a interface inicial. Opcao para abrir arquivos adicionada.

--- application.py
+++ application.py
@@ -1,16 +1,53 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from tkinter import *
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
-class Application:
-	"""docstring for Application"""
+import Tkinter
+import Tkconstants
+import tkFileDialog
+from file_manager import *
 
-	def __init__(self, toplevel):
-		pass
+class Application(Tkinter.Frame):
+	"""Application e a classe responsavel pela Interface do ID3"""
+
+	def __init__(self, root):
+
+		Tkinter.Frame.__init__(self, root)
+
+		# Opcoes para Buttons
+		button_opt = {'fill': Tkconstants.BOTH, 'padx': 5, 'pady': 5}
+
+		# Define Buttons
+		Tkinter.Button(self, text='Abrir arquivo...', command=self.open_file).pack(**button_opt)
+		
+		# Define as opcoes para abrir um arquivo
+		self.file_opt = options = {}
+		options['defaultextension'] = '.csv'
+		options['filetypes'] = [('all files', '.*'), ('csv files', '.csv')]
+		options['initialdir'] = 'C:\\'
+		options['parent'] = root
+		options['title'] = 'This is a title'
+
+	def open_file(self):
+
+		"""
+		Abre um File Dialog que retorna o nome do arquivo.
+		"""
+		# get filename
+		filename = tkFileDialog.askopenfilename(**self.file_opt)
+
+		# open file on your own
+		if filename:
+		  	fm = FileManager()
+
+			# Le os dados de entrada a partir de um arquivo csv
+			file_content = fm.read_csv(filename)
 
 
-if __name__ == '__main__':
-	raiz = Tk()
-	Application(raiz)
-	raiz.mainloop()		
+if __name__=='__main__':
+	root = Tkinter.Tk()
+	Application(root).pack()
+	root.mainloop()
+
