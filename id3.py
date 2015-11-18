56cc914743aad3700ea0111eaaf1e34f15327820
Luis Guilherme <luisguilherme.ufpi@gmail.com>
2015-11-18 12:19:51 -0300

    Feita funcao para imprimir Node.

--- id3.py
+++ id3.py
@@ -21,11 +21,82 @@ class ID3:
 
 		print('\n')
 
-		# Testando Node
-		for index, at in enumerate(attributes):
-			node = Node(at)
-			node.setChildrens(self.addChildrens(self.occurrences, index))
-			print(node)
+		# Testando Node -----------------------------------------------------
+		raiz = Node('MONTANTE')
+		childrensRaiz = []
+
+		node1 = Node('SALARIO')
+		key1 = {}
+		key1['medio'] = node1
+		childrensRaiz.append(key1)
+
+		node2 = Node('sim')
+		key2 = {}
+		node2.setChildrens
+		key2['baixo'] = node2
+		childrensRaiz.append(key2)
+
+		node3 = Node('CONTA')
+		key3 = {}
+		key3['alto'] = node3
+		childrensRaiz.append(key3)
+
+		raiz.setChildrens(childrensRaiz)
+
+		childrens1 = []
+
+		node4 = Node('nao')
+		key4 = {}
+		key4['<= 990'] = node4
+		childrens1.append(key4)
+
+		node5 = Node('sim')
+		key5 = {}
+		key5['> 990'] = node5
+		childrens1.append(key5)
+
+		node1.setChildrens(childrens1)
+
+
+		childrens3 = []
+
+		node6 = Node('nao')
+		key6 = {}
+		key6['nao'] = node6
+		childrens3.append(key6)
+
+		node7 = Node('sim')
+		key7 = {}
+		key7['sim'] = node7
+		childrens3.append(key7)
+
+		node3.setChildrens(childrens3)
+
+		print(raiz)
+
+
+		# for index, at in enumerate(attributes):
+		# 	node = Node(at)
+
+		# 	childrens = self.addChildrens(self.occurrences, index)
+		# 	node.setChildrens(childrens)
+
+		# 	if index == 1:
+		# 		childrens[1]['senior'].setName('test')
+
+		# 	 	node1 = Node()
+		# 	 	key = {}
+		# 	 	childrensTest = []
+		# 	 	key['testArrest'] = node1
+		# 	 	childrensTest.append(key)
+		# 	 	childrens[1]['senior'].setChildrens(childrensTest)
+		# 	 	#print(childrens[1]['senior'])
+		# 	 	#print('(' + childrens[1]['senior'] + ')')
+				
+		# 	print(node)
+
+		# --------------------------------------------------------------------
+
 	
 
 	def execute(self):
--- node.py
+++ node.py
@@ -8,16 +8,23 @@ class Node:
 		"""
 
 		self.name = name
-		self.childrens = []
+		self.childrens = []	# Lista de dicionarios contendo [{'aresta', No}]
 
 
 	def setChildrens(self, childrens):
 		"""
 		Adiciona a lista de filhos de cada Node
 		"""
-
 		self.childrens = childrens
 
+	def getChildrens(self):
+		return self.childrens
+
+	def getName(self):
+		return self.name
+
+	def setName(self, name):
+		self.name = name
 
 	def __str__(self):
 		"""
@@ -25,13 +32,30 @@ class Node:
 		"""
 
 		nodeStr = ""
+		space = "  "
 
-		nodeStr += self.name + ' | '
+		nodeStr += self.name + '\n'
 
 		for index in range(len(self.childrens)):
-			nodeStr += self.childrens[index].keys()[0]
-			if index != len(self.childrens) - 1:
-				nodeStr += ' - '
+			nodeStr += space + '|_ (' + self.childrens[index].keys()[0] + ') '
+			nodeStr += self.strAux(self.childrens[index].values()[0], (space + "  "))
 
 		nodeStr += '\n'
 		return nodeStr
+
+
+	def strAux(self, node, spaceAux):
+		"""
+		
+		"""
+		strReturn = ""
+
+		if node.getName() is not None:
+			strReturn += node.getName() + '\n'
+			for i in range(len(node.getChildrens())):
+				strReturn += spaceAux + '|_ (' + node.getChildrens()[i].keys()[0] + ') '
+				if node.getChildrens()[i].values()[0] is not []:
+					strReturn += self.strAux(node.getChildrens()[i].values()[0], (spaceAux + "  "))
+
+
+		return strReturn
