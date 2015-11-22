1f793d6f06a72a02ed53a7fdcc54a330bc5c6553
Hugo Piauilino <hugos94@gmail.com>
2015-11-22 19:54:57 -0300

    Decision tree comentada

--- decision_tree.py
+++ decision_tree.py
@@ -5,25 +5,45 @@ from node import *
 from graphviz import Digraph
 
 class DecisionTree(object):
+	"""docstring for DecisionTree"""
 	global dot
 
-	"""docstring for DecicionTree"""
 	def __init__(self, tree):
+		"""Construtor da classe DecisionTree"""
+
 		super(DecisionTree, self).__init__()
+
 		self.tree = tree
+
 		global dot
 		dot = Digraph()
 		dot.attr('node', shape='circle')
+
+		# Chama o metodo de criacao da arvore de decisao
 		self.build_tree(tree)
+
+		# Renderiza a arvore de decisao
 		dot.view()
 
+
 	def build_tree(self, node):
+		"""Metodo que percorre os nos e retorna a representacao grafica da arvore de decisao"""
+
 		global dot
+
+		# Verifica se o no e nulo
 		if node.get_label() is not None:
+			# Cria o no na interface
 			dot.node(node.get_label(), node.get_label())
+			# Verifica se o no tem filhos
 			if node.get_childrens() is not []:
+				# Cria uma lista de filhos
 				childrens = node.get_childrens()
+				#Percorre o dicionario de filhos
 				for child in childrens.keys():
-					label = childrens.get(child)
+					# Cria um no filho
+					child_label = childrens.get(child)
+					# Chama o metodo pra criar a arvore de decisao com o no filho
 					self.build_tree(childrens.get(child))
-					dot.edge(node.get_label(),label.get_label(), label=child)
+					# Cria a ligacao entre o no pai e o no filho na interface
+					dot.edge(node.get_label(), child_label.get_label(), label=child)
