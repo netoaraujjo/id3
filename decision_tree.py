e8bed05137650120d30ee43e52525631e4a984f4
Hugo Piauilino <hugos94@gmail.com>
2015-11-22 19:05:11 -0300

    Introduzida a criacao da representacao grafica da arvore de decisao

--- decision_tree.py
+++ decision_tree.py
@@ -2,12 +2,28 @@
 # -*- coding: utf-8 -*-
 
 from node import *
+from graphviz import Digraph
 
 class DecisionTree(object):
+	global dot
+
 	"""docstring for DecicionTree"""
 	def __init__(self, tree):
 		super(DecisionTree, self).__init__()
 		self.tree = tree
+		global dot
+		dot = Digraph()
+		dot.attr('node', shape='circle')
+		self.build_tree(tree)
+		dot.view()
 
-	def build_tree(self):
-		pass
+	def build_tree(self, node):
+		global dot
+		if node.get_label() is not None:
+			dot.node(node.get_label(), node.get_label())
+			if node.get_childrens() is not []:
+				childrens = node.get_childrens()
+				for child in childrens.keys():
+					label = childrens.get(child)
+					self.build_tree(childrens.get(child))
+					dot.edge(node.get_label(),label.get_label(), label=child)
